96baa66b038439f8c0b04dff60ad5ca5d104b26c
Michał <33964835+DonTheDevPL@users.noreply.github.com>
2020-07-28 20:52:00 +0200

    base config, debug textbox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,16 +23,57 @@ namespace Arma3FactionGenerator
         public MainWindow()
         {
             InitializeComponent();
-            string[] sideArray = { "BLUFOR", "INDFOR", "OPFOR", "CIV" };
+            string[] sideArray = { "OPFOR", "BLUFOR", "INDFOR" };
             foreach (string side in sideArray)
             {
                 sideListBox.Items.Add(side);
             }
+            sideListBox.SelectedIndex = 0;
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private string generateFaction(string FunNameClass, string FunName, string Author, int side)
         {
+            string config = $@"class CfgPatches
+{{
+    class {FunNameClass}
+    {{
+        units[]= {{}};
+        weapons[]={{}};
+        requiredVersion = 0.1;
+        requiredAddons[] = {{}};
+        author = ""{Author}"";
+    }};
+}};
+class cfgFactionClasses
+{{
+    class {FunNameClass}_faction
+    {{
+        displayName = ""{FunName}"";
+        priority = 3;
+        side = {side};
+        icon = """";
+    }};
+}};
+class cfgVehicleClasses
+{{
+    class {FunNameClass}_Men     
+    {{
+        displayName=""Men"";
+    }};
+}};";
+            return config;
 
         }
+
+        private void generateConfig(object sender, RoutedEventArgs e)
+        {
+            string FunNameClass = fnc.Text;
+            string FunName = fn.Text;
+            string Author = auth.Text;
+            int side = sideListBox.SelectedIndex;
+            string finall = generateFaction(FunNameClass, FunName, Author, side);
+            debug.Text = finall;
+        }
+
     }
 }
